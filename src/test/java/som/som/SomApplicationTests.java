e6e4f1d023a3c089d04b374f1772c7cbcf5a6487
0510jian <0510jian@gmail.com>
2023-06-14 17:17:41 +0900

    init: Create project

---
+++ src/test/java/som/som/SomApplicationTests.java
@@ -0,0 +1,13 @@
+package som.som;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
